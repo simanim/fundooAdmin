b763c51cf6b98d9006679319069905aa63c0511a
simanim <simanimeher@gmail.com>
2018-12-14 18:06:24 +0530

    14/12

--- src/app/component/cart-list/cart-list.component.ts
+++ src/app/component/cart-list/cart-list.component.ts
@@ -48,30 +48,42 @@ export class CartListComponent implements OnInit {
       },
       
       success: function (data) {
+        console.log(data);
+        
+        var rowindex;
         var cartList=[];
         for(let i=0;i<data['data'].length;i++){
-          cartList.push([i+1,data['data'][i].id,data['data'][i].product.name,data['data'][i].price])
+          if(data['data'][i].user)
+            cartList.push([i+1,data['data'][i].user.firstName+" "+data['data'][i].user.lastName,data['data'][i].user.addresses[0].address,data['data'][i].product.name])
+          else
+            cartList.push([i+1,"----","----",data['data'][i].product.name])
+
         }
         $("#loader").hide();
         $("#cartList").show();
 
         var table=$('#cartList').DataTable( {
           "data":cartList,
+          "columns":[
+            {'width':'10%'},{'width':'20%'},{'width':'30%'},{'width':'20%'},{'width':'20%'}],
           "columnDefs": [{
             "targets": 4,
             "render": function ( data, type, row, meta ) {
-              return '<button type="button" class="btn btn-outline-success" id="place" style="background-color:rgb(203, 241, 230)">place order</button>';
+              return '<button type="button" class="btn btn-outline-success" id="place" style="background-color:rgb(203, 241, 230)">confirm</button><button type="button" class="btn btn-outline-success" id="cancel" style="background-color:rgb(203, 241, 230)">cancel</button>';
             }
           }]
         });
 
-        $('#cartList tbody').on('click', 'tr', function () {
-          rowIndex = table.row(this).index();
+
+        $('#cartList tbody').on('click', '#place', function () {
+          rowindex=$(this).closest('tr')
+          var index=table.row(rowindex).data()[0]-1;
+          
           $.ajax({
             type: 'POST',
             url: 'http://34.213.106.173/api/productcarts/adminCompleteOrder',
             data:{
-              "cartId": data['data'][rowIndex].id
+              "cartId": data['data'][index].id
             },
             dataType:"json",
             headers:{
@@ -84,6 +96,48 @@ export class CartListComponent implements OnInit {
             }
           })
         });
+
+        $('#cartList tbody').on('click', '#cancel', function () {
+          rowindex=$(this).closest('tr')
+          var index=table.row(rowindex).data()[0]-1;
+          
+          $.ajax({
+            type: 'POST',
+            url: 'http://34.213.106.173/api/productcarts/adminCancelOrder',
+            data:{
+              "cartId": data['data'][index].id
+            },
+            dataType:"json",
+            headers:{
+              "Authorization": id
+            },
+            success: function (data) {
+              window.location.href = "/cart";
+            },
+            error: function (request, status, error) {
+            }
+          })
+        });
+
+        // $('#cartList tbody').on('click', 'tr', function () {
+        //   rowIndex = table.row(this).index();
+        //   $.ajax({
+        //     type: 'POST',
+        //     url: 'http://34.213.106.173/api/productcarts/adminCompleteOrder',
+        //     data:{
+        //       "cartId": data['data'][rowIndex].id
+        //     },
+        //     dataType:"json",
+        //     headers:{
+        //       "Authorization": id
+        //     },
+        //     success: function (data) {
+        //       window.location.href = "/cart";
+        //     },
+        //     error: function (request, status, error) {
+        //     }
+        //   })
+        // });
       },
       error: function (request, status, error) {
       }
